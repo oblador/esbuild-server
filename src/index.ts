f144fb39003ae9321ea7765cf2302a2ec8ac832d
Dara Dermody <daradermody@gmail.com>
2023-04-24 13:06:31 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -236,7 +236,7 @@ export function createServer(
       ).on('error', (err) => {
         const msg = `Error connecting to the proxy via ${rewrite}`;
         console.error(msg, err);
-        res.writeHead(500).write(msg);
+        res.writeHead(500, { 'Content-Type': 'text/plain' }).end(msg);
       });
     }
 
