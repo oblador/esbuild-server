3594a7030955ac3de10c45e347c8cb4da33a9bb4
Dara Dermody <daradermody@gmail.com>
2023-04-26 09:40:35 +0100

    Prevent exiting on error connecting to proxy (#7)
    * Prevent exiting on error connecting to proxy
    
    * Update index.ts
    
    * Update src/index.ts
    
    Co-authored-by: Joel Arvidsson <joel@oblador.se>
    
    ---------
    
    Co-authored-by: Joel Arvidsson <joel@oblador.se>

--- src/index.ts
+++ src/index.ts
@@ -233,7 +233,11 @@ export function createServer(
           }
         ),
         { end: true }
-      );
+      ).on('error', (err) => {
+        const msg = `Error connecting to the proxy via ${rewrite}`;
+        console.error(msg, err);
+        res.writeHead(502, { 'Content-Type': 'text/plain' }).end(msg);
+      });
     }
 
     // Stall request while rebuilding to not serve stale files
